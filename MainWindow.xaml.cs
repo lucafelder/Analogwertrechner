c9f1e00929cef27660e9261a8caa5352352a3b9b
Luca Felder <luca.felder@stud.hslu.ch>
2025-02-22 08:55:11 +0100

    implement input validation and factor calculation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Analogwertrechner;
 
@@ -19,5 +11,51 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        FaktorCalc(txtBoxMinWertSps, txtBoxMaxWertSps, txtBoxMesswertSps, txtFaktorSps, "SPS", _factorSps);
+        FaktorCalc(txtBoxMinWertAnalog, txtBoxMaxWertAnalog, txtBoxMesswertAnalog, txtFaktorAnalog, "Analog", _factorAnalog);
+        FaktorCalc(txtBoxMinWertReal, txtBoxMaxWertReal, txtBoxMesswertReal, txtFaktorReal, "Real", _factorRealWorld);
+    }
+
+    private void SpsWert_lostFocus(object sender, RoutedEventArgs e)
+    {
+        FaktorCalc(txtBoxMinWertSps, txtBoxMaxWertSps, txtBoxMesswertSps, txtFaktorSps, "SPS", _factorSps);
+    }
+
+    private void AnalogWert_lostFocus(object sender, RoutedEventArgs e)
+    {
+        FaktorCalc(txtBoxMinWertAnalog, txtBoxMaxWertAnalog, txtBoxMesswertAnalog, txtFaktorAnalog, "Analog", _factorAnalog);
+    }
+
+    private void RealerWert_lostFocus(object sender, RoutedEventArgs e)
+    {
+        FaktorCalc(txtBoxMinWertReal, txtBoxMaxWertReal, txtBoxMesswertReal, txtFaktorReal, "Real", _factorRealWorld);
+    }
+
+    private float _factorSps, _factorAnalog, _factorRealWorld;
+
+    private void FaktorCalc(TextBox txtBoxMinRange, TextBox txtBoxMaxRange, TextBox txtBoxValue, TextBlock txtBoxFactor, string category, float factor)
+    {
+        try
+        {
+            if (txtBoxMinRange.Text == "") throw new ArgumentException($"{category} Bereich Min ohne Eingabe");
+            if (txtBoxMaxRange.Text == "") throw new ArgumentException($"{category} Bereich Max ohne Eingabe");
+            if (txtBoxValue.Text == "") throw new ArgumentException($"{category} Messwert ohne Eingabe");
+
+            float minRange, maxRange, value;
+
+            if (!float.TryParse(txtBoxMinRange.Text, out minRange)) throw new ArgumentException($"{category} Bereich Min ist keine Zahl");
+            if (!float.TryParse(txtBoxMaxRange.Text, out maxRange)) throw new ArgumentException($"{category} Bereich Max ist keine Zahl");
+            if (!float.TryParse(txtBoxValue.Text, out value)) throw new ArgumentException($"{category} Messwert ist keine Zahl");
+
+            if (minRange > maxRange || minRange < 0 || minRange > value || minRange == maxRange)
+                throw new ArgumentException($"{category} Wert/Grenzen haben eine ungültige konstelation");
+
+            factor = (value - minRange) / (maxRange - minRange);
+            txtBoxFactor.Text = string.Format("{0:N2}", factor);
+        }
+        catch
+        {
+            txtBoxFactor.Text = "?";
+        }
     }
 }
